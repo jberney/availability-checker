e0eb79ebd0744cc016d43f4231e45c4d71873926
Jonathan Berney <jberney@users.noreply.github.com>
2020-10-26 00:24:08 -0700

    monitor bestbuy/bhphoto for 3090s

--- index.js
+++ index.js
@@ -1,106 +1,73 @@
-const request = require('request-promise');
-const {RestClient} = require('twilio');
+/*
+Amazon
+NewEgg
+Overclockers?
+ */
 
-const twilioClient = new RestClient(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+require('dotenv').config();
+const {BEST_BUY_API_KEY, TWILIO_FROM, TWILIO_TO} = process.env;
+const bestbuy = require('bestbuy')(BEST_BUY_API_KEY);
+const twilio = require('twilio')();
+const fetch = require('node-fetch');
 
-function clean(text) {
-  return text.split('').filter(c => c.charCodeAt(0) < 128).join('')
+const startInterval = (cb, ms) => {
+  cb();
+  return setInterval(cb, ms);
 }
-
-const shortUrls = {};
-const bestBuyCache = {};
-const walmartCache = {};
-
-function filterItem(itemId, status, unavailableStatuses, cache) {
-  let pass = false;
-  const oldStatus = cache[itemId];
-  if (oldStatus) {
-    pass = oldStatus !== status;
-  } else {
-    pass = unavailableStatuses.indexOf(status) === -1;
-  }
-  cache[itemId] = status;
-  return pass;
-}
-
-async function shortenUrl(longUrl) {
-  if (!shortUrls[longUrl]) {
-    try {
-      const {id} = await request.post({
-        url: 'https://www.googleapis.com/urlshortener/v1/url',
-        qs: {key: process.env.GOOGLE_API_KEY},
-        body: {longUrl},
-        json: true
-      });
-      shortUrls[longUrl] = id;
-    } catch (e) {
-      console.error(e);
-    }
-  }
-  return shortUrls[longUrl] || longUrl;
-}
-
-async function sendMessage(body) {
-  return new Promise(res => {
-    twilioClient.sms.messages.create({
-      from: process.env.TWILIO_NUMBER,
-      to: process.env.TO_NUMBER,
-      body
-    }, (error, message) => {
-      console.log(`[${new Date()}] ${body}`);
-      if (!error) return res(message);
-      console.error(error);
-      res(error);
-    });
-  });
-}
-
-function check({url, apiKey, qs, itemsKey, idKey, stockKey, unavailableStatuses, itemCache, interval}) {
-  setInterval(async() => {
-    try {
-      const response = await request({
-        url,
-        qs: Object.assign({apiKey, format: 'json'}, qs),
-        json: true
-      });
-      const items = response[itemsKey];
-      const changed = items
-        .filter(({marketplace}) => !marketplace)
-        .filter(item => filterItem(item[idKey], item[stockKey], unavailableStatuses, itemCache));
-      changed.forEach(async item => {
-        const stock = item[stockKey];
-        const {name, addToCartUrl} = item;
-        let message = `${stock} ${clean(name)}`;
-        const available = unavailableStatuses.indexOf(stock) === -1;
-        if (available) message = `${message} "${await shortenUrl(addToCartUrl)}"`;
-        await sendMessage(message);
-      });
-    } catch ({message}) {
-      console.error(message);
-    }
-  }, interval * 1000);
-}
-
-check({
-  url: `https://api.bestbuy.com/v1/products(sku in (${[5670003, 5670100].join(',')}))`,
-  apiKey: process.env.BEST_BUY_API_KEY,
-  qs: {show: ['sku', 'name', 'orderable', 'addToCartUrl'].join(',')},
-  itemsKey: 'products',
-  idKey: 'sku',
-  stockKey: 'orderable',
-  unavailableStatuses: ['ComingSoon', 'NotOrderable', 'SoldOut'],
-  itemCache: bestBuyCache,
-  interval: 30
+const fetchJson = (...args) => fetch(...args).then(response => response.json());
+const postJson = (url, opts) => fetchJson(url, {
+  ...opts,
+  body: JSON.stringify(opts.body),
+  headers: {'content-type': 'application/json', ...opts.headers},
+  method: 'POST'
 });
+const sendMessage = body => twilio.messages.create({body, from: TWILIO_FROM, to: TWILIO_TO});
 
-check({
-  url: 'http://api.walmartlabs.com/v1/items',
-  apiKey: process.env.WALMART_API_KEY,
-  qs: {ids: [52901821, 55449981].join(',')},
-  itemsKey: 'items',
-  idKey: 'itemId',
-  stockKey: 'stock',
-  unavailableStatuses: ['Not available'],
-  itemCache: walmartCache,
-  interval: 20
-});
+const intervals = {
+  bestbuy: 30000,
+  bhphoto: 30000
+};
+
+const skus = {
+  bestbuy: [
+    6429434, // nvidia fe
+    6432446, // asus tuf
+    6432447, // asus strix
+    6434363, // evga xc3
+    6436192, // evga ftw3 ultra
+    6436193, // evga ftw3
+  ],
+  bhphoto: [
+    1434933, // evga ftw3 ultra
+    1594384, // asus tuf
+    1594385, // asus tuf oc
+    1594451, // asus tuf oc
+    1594454, // asus tuf
+  ]
+};
+
+const apis = {
+  bestbuy: {
+    getItems: () => bestbuy.products(`sku in (${skus.bestbuy.join(',')})`,
+      {show: ['manufacturer', 'modelNumber', 'onlineAvailability', 'addToCartUrl'].join(',')})
+      .then(({products}) => products
+        .filter(({onlineAvailability}) => onlineAvailability)),
+    toString: p => `${p.manufacturer} ${p.modelNumber}: ${p.addToCartUrl}`
+  },
+  bhphoto: {
+    getItems: () => postJson('https://www.bhphotovideo.com/api/item/p/product-details', {
+      body: {
+        params: {
+          itemList: skus.bhphoto.map(skuNo => ({skuNo, itemSource: 'REG'})),
+          channels: ['priceInfo'],
+          channelParams: {priceInfo: {PRICING_CONTEXT: 'DETAILS_CART_LAYER'}}
+        }
+      }
+    }).then(({data}) => data.filter(p => p.priceInfo.addToCartFunction)),
+    toString: p => `${p.core.brandSeriesModel}: ${p.core.bitlyUrl}`
+  }
+};
+
+Object.entries(apis).forEach(([api, {getItems = () => [], toString}]) => startInterval(
+  () => getItems().then(ps => ps.map(toString).forEach(sendMessage)),
+  intervals[api]));
