01318b522b56f8819cd197659579b92563f9c3bc
Jonathan Berney <jberney@users.noreply.github.com>
2020-10-26 00:40:24 -0700

    startInterval

--- index.js
+++ index.js
@@ -7,12 +7,8 @@ Overclockers?
 require('dotenv').config();
 const bestbuy = require('./lib/bestbuy');
 const bhphoto = require('./lib/bhphoto');
+const {startInterval} = require('./lib/javascript');
 const {sendMessage} = require('./lib/twilio');
-
-const startInterval = (cb, ms) => {
-  cb();
-  return setInterval(cb, ms);
-}
 
 [bestbuy, bhphoto].forEach(({getItems = () => [], toString, interval}) => startInterval(
   () => getItems().then(ps => ps.map(toString).forEach(sendMessage)),
